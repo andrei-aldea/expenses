4e75e4933a0994308e4552e9ef4d2e1c9e082541
Andrei Aldea <fn7x@icloud.com>
2023-02-17 00:38:07 +0200

    basic nav

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -19,13 +19,21 @@ const NavLinks: Array<NavItem> = [
 export default function Navigation() {
 	return (
 		<nav>
-			<ul className='hidden md:flex h-screen bg-neutral-200 p-6 flex-col gap-4'>
+			<ul className='hidden h-screen flex-col gap-5 border-r border-neutral-300 bg-neutral-200 p-6 drop-shadow-md md:flex'>
+				<li className=' select-none text-4xl font-semibold'>
+					E
+					<span className='bg-gradient-to-r from-black to-amber-600 bg-clip-text text-transparent'>
+						xpenses
+					</span>
+				</li>
 				{NavLinks.map((item, index) => (
-					<li key={index} className='w-40'>
+					<li key={index}>
 						<NavLink
 							to={item.to}
 							className={({ isActive }) =>
-								isActive ? 'bg-neutral-300 rounded-md p-2' : 'p-2'
+								isActive
+									? 'rounded-md bg-neutral-300 p-2 text-lg font-bold'
+									: 'p-2 text-lg font-medium'
 							}
 						>
 							{item.name}
