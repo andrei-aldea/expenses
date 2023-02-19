e1c2646529a45e1db172859e954cf931df0f23e5
Andrei Aldea <fn7x@icloud.com>
2023-02-19 22:07:57 +0200

    replacing favicon

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -5,7 +5,7 @@ export default function AuthPage() {
 		<section className='grid grid-cols-3 gap-4'>
 			<AuthForm />
 			<div className='col-span-3 h-full w-full rounded-md bg-neutral-200 p-4 font-bold sm:col-span-1'>
-				Use an account in order to use the dashboard and all of it's features
+				Use an account in order to use the dashboard and all of it's features.
 			</div>
 		</section>
 	)
